61cc4502b4e003b039ce6b814675aec8ec6628ce
Jasur <jasurnabijoniv@gmail.com>
2026-06-25 00:22:05 +0500

    feat: дописал все CRUD эндпоинты в EmployeeController

--- src/main/java/org/jsr/mvc/bookingapp/controller/EmployeeController.java
+++ src/main/java/org/jsr/mvc/bookingapp/controller/EmployeeController.java
@@ -1,4 +1,39 @@
 package org.jsr.mvc.bookingapp.controller;
 
+import org.jsr.mvc.bookingapp.dto.request.EmployeeRequest;
+import org.jsr.mvc.bookingapp.dto.response.EmployeeResponse;
+import org.jsr.mvc.bookingapp.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    public EmployeeResponse create(@RequestBody EmployeeRequest employeeRequest) {
+        return employeeService.create(employeeRequest);
+    }
+
+    @GetMapping("/{id}")
+    public EmployeeResponse findById(@PathVariable Long id) {
+        return employeeService.findById(id);
+    }
+
+    @GetMapping
+    public List<EmployeeResponse> findAll() {
+        return employeeService.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        employeeService.delete(id);
+    }
 }
